a8d2ac65d8bf667aaf81b220c698894eae08f8ff
Samuel Silva <samuelslv7@gmail.com>
2024-05-23 22:16:08 -0300

    corrigindo a interface inicial #27

--- Fronted/src/App.js
+++ Fronted/src/App.js
@@ -1,14 +1,27 @@
 // src/App.js
-import React from 'react';
-import FormularioCadastro from './componentes/FormularioCadastro';
+import React from "react";
+// import FormularioCadastro from "./componentes/FormularioCadastro";
+import "normalize.css";
+import Inicial from "./componentes/inicial"; // Importe o componente com a primeira letra maiúscula
 
 const App = () => {
   return (
-    <div className="App">
-      <h1>Formulário de Cadastro</h1>
-      <FormularioCadastro />
-    </div>
+    <Inicial />
+    // <div className="App">
+    //   <h1>Formulário de Cadastro</h1>
+    //   <FormularioCadastro />
+    // </div>
   );
 };
+
+//  function App() {
+//    return (
+//      <div className="App">
+//        {/* <Inicial /> */}
+//        <h1>Formulário de Cadastro</h1>
+//        <FormularioCadastro />
+//      </div>
+//    );
+//  }
 
 export default App;
