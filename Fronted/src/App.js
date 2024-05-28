5e70bdc79c416df5d363cdf7c73f1eabaa6a7b55
Bianca Bastos <biancabsouza23@gmail.com>
2024-05-28 01:48:41 -0300

    correcoes iniciais #37

--- Fronted/src/App.js
+++ Fronted/src/App.js
@@ -1,9 +1,13 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./componentes/normalize.css";
 import PaginaInicial from "./componentes/inicial"; // Importe o componente com a primeira letra maiúscula
+import EditPerfil from "./componentes/EditPerfil";
+import FormularioCadastro from "./componentes/FormularioCadastro";
+import MeuPerfil from "./componentes/MeuPerfil";
 
 function App() {
-  return <PaginaInicial />;
+  return <MeuPerfil />;
 }
 
 export default App;
