c6d8f7f63f90e5e685f91320851f9c34064b56ee
Bianca Bastos <biancabsouza23@gmail.com>
2024-07-16 09:21:42 -0300

    Adicionar mapa de geolocalização na tela de cadastro #7

--- Fronted/src/componentes/FormularioCadastro.js
+++ Fronted/src/componentes/FormularioCadastro.js
@@ -1,6 +1,16 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import { useNavigate } from 'react-router-dom'; // Importa useNavigate
+import styled from 'styled-components';
+import L from 'leaflet';
+import 'leaflet/dist/images/marker-icon.png';
+import 'leaflet/dist/images/marker-icon-2x.png';
+import 'leaflet/dist/images/marker-shadow.png';
 import './FormularioCadastro.css';
+
+const StyledMapContainer = styled(MapContainer)`
+  min-height: 400px; // Define o height mínimo como 400 pixels
+`;
 
 const FormularioCadastro = () => {
   const [formData, setFormData] = useState({
@@ -8,13 +18,16 @@ const FormularioCadastro = () => {
     username: '',
     email: '',
     password: '',
-    latitude: null,
-    longitude: null,
+    latitude: 5,
+    longitude: 1,
   });
+
+  const mapRef = useRef(null);
+  const [position, setPosition] = useState([5, 1]);
 
   const navigate = useNavigate(); // Inicializa o useNavigate
 
-  useEffect(() => {
+  const updateLocation = () => {
     if ("geolocation" in navigator) {
       navigator.geolocation.getCurrentPosition((position) => {
         setFormData((prevFormData) => ({
@@ -22,10 +35,16 @@ const FormularioCadastro = () => {
           latitude: position.coords.latitude,
           longitude: position.coords.longitude,
         }));
+        setPosition([position.coords.latitude, position.coords.longitude]);
+        mapRef.current.setView([position.coords.latitude, position.coords.longitude], 13);
       });
     } else {
       console.log("Geolocalização não é suportada neste navegador.");
     }
+  }
+
+  useEffect(() => {
+    updateLocation(); 
   }, []);
 
   const handleChange = (e) => {
@@ -44,6 +63,12 @@ const FormularioCadastro = () => {
     // Redireciona para a página de perfil após o envio do formulário
     navigate('/meu-perfil');
   };
+
+  const handleRefreshLocation = (e) => {
+    e.preventDefault();
+    updateLocation();
+  };
+
 
   return (
     <form onSubmit={handleSubmit}>
@@ -86,6 +111,21 @@ const FormularioCadastro = () => {
           value={formData.password}
           onChange={handleChange}
         />
+      </div>
+      <div>
+        <h3>Localização</h3>
+        <button onClick={handleRefreshLocation}>Atualizar</button>
+        <StyledMapContainer center={position} zoom={13} scrollWheelZoom={false} ref={mapRef}>
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <Marker position={position}>
+            <Popup>
+              A sua localização estimada.
+            </Popup>
+          </Marker>
+        </StyledMapContainer>
       </div>
       <button type="submit">Cadastre-se</button>
     </form>
